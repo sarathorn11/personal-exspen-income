26bbd5793667bcbd5f0fb8f4ec3b8f3b93d2aed4
sarath.orn <sarath.orn@z1fsc.com>
2023-05-18 09:50:49 +0700

    add new

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 TYPE = (
-    ('Postive','Postive'),
+    ('Positive','Positive'),
     ('Negative','Negative'),
 )
 
@@ -17,7 +17,9 @@ class Profile(models.Model):
 
 
 class Expense(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    amount = models.FloatField()
     expense_type = models.CharField(max_length=100,choices=TYPE)
     
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -5,13 +5,22 @@ from .models import *
 
 
 def home(request):
+    profile = Profile.objects.filter(user=request.user).first()
+    expenses = Expense.objects.filter(user = request.user)
     if request.method =='POST':
         text = request.POST.get('text')
         amount = request.POST.get('amount')
         expense_type = request.POST.get('expense_type')
-        print(text,amount,expense_type)
+        
+        expense = Expense(name=text, amount=amount, expense_type=expense_type,user=request.user)
+        expense.save()
+        if expense_type == 'Positive':
+            profile.balance += float(amount) 
+        else: 
+            profile.expenses += float(amount) 
+            profile.balance -= float(amount) 
+        profile.save()
     
-    profile = Profile.objects.filter(user=request.user).first()
     
-    context = {'profile': profile}
+    context = {'profile': profile,'expenses': expenses}
     return render(request,'home.html',context)
